30e7b90883e45d59beaa25ccfa76766a06664011
Block-Expert <shigeoh56@gmail.com>
2022-11-04 00:13:39 +0800

    add minimalproxy and change delegatecall

--- test/DelegateCall.test.ts
+++ test/DelegateCall.test.ts
@@ -14,22 +14,42 @@ describe("DelegateCallTest", function () {
     [owner, otherAccount] = await ethers.getSigners();
     const B = await ethers.getContractFactory("B");
     b = await B.deploy();
+    await b.deployed();
   
     const A = await ethers.getContractFactory("A");
-    a = await A.deploy();
+    a = await A.deploy(b.address);
   })
 
-  it("test_A_setVars", async function () {
-    const tx = await a.setVars(b.address, 10, {value: "20"});
+  it("test_A_fallback_setVarB", async function () {
+
+    const bA = await ethers.getContractAt('B', a.address)
+
+    const tx = await bA.setVars(10, {value: "20"});
+    
+    expect(await bA.num()).to.be.equal(10);
+    expect(await bA.sender()).to.be.equal(owner.address);
+    expect(await bA.value()).to.be.equal(20);
     
     expect(await a.num()).to.be.equal(10);
+    expect(await a.value()).to.be.equal(20);
+
+    expect(await b.num()).to.be.equal(0);
+    expect(await b.value()).to.be.equal(0);
+
+    // expect(await b.num()).to.be.equal(0);
+    // expect(await b.sender()).to.be.equal(a.address);
+    // expect(await b.value()).to.be.equal(10); //???????
+    // expect(await ethers.provider.getBalance(a.address)).to.be.equal(10);
+    // expect(await ethers.provider.getBalance(b.address)).to.be.equal(10);
+  });
+
+  it("test_A_setVarA", async function () {
+    await a.setVarsA(b.address, 50, {value: "20"});
+    expect(await a.num()).to.be.equal(50);
     expect(await a.sender()).to.be.equal(owner.address);
     expect(await a.value()).to.be.equal(20);
-    
-    expect(await b.num()).to.be.equal(50);
-    expect(await b.sender()).to.be.equal(a.address);
-    expect(await b.value()).to.be.equal(10); //???????
-    expect(await ethers.provider.getBalance(a.address)).to.be.equal(10);
-    expect(await ethers.provider.getBalance(b.address)).to.be.equal(10);
+
+    expect(await b.num()).to.be.equal(0);
+    expect(await b.value()).to.be.equal(0);
   });
 });
